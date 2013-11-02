5e42d6dd6bdfbe2b97e56ec2f81d306a3c545e1a
yamayo <yamatoyo@gmail.com>
2013-11-02 10:47:48 +0900

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -59,20 +59,20 @@ node default {
   include nginx
 
   # fail if FDE is not enabled
-  if $::root_encrypted == 'no' {
-    fail('Please enable full disk encryption and try again')
-  }
+  #if $::root_encrypted == 'no' {
+  #  fail('Please enable full disk encryption and try again')
+  #}
 
   # node versions
-  include nodejs::v0_4
-  include nodejs::v0_6
-  include nodejs::v0_8
+  #include nodejs::v0_4
+  #include nodejs::v0_6
+  #include nodejs::v0_8
   include nodejs::v0_10
 
   # default ruby versions
-  include ruby::1_8_7
-  include ruby::1_9_2
-  include ruby::1_9_3
+  #include ruby::1_8_7
+  #include ruby::1_9_2
+  #include ruby::1_9_3
   include ruby::2_0_0
 
   # common, useful packages
